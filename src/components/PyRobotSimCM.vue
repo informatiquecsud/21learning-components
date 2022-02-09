18941cedb7c59ddbd59ae283fd6f38807d70269c
Cédric Donner <cedonner@gmail.com>
2022-02-09 09:34:26 +0100

    suite pyrobotsim

--- src/components/PyRobotSimCM.vue
+++ src/components/PyRobotSimCM.vue
@@ -3,6 +3,14 @@
     <template v-slot:before>
       <q-header elevated>
         <q-toolbar>
+          <q-btn color="green" class="q-ma-sm" @click="runCode">Exécuter</q-btn>
+          <q-btn
+            color="white"
+            text-color="black"
+            class="q-ma-sm"
+            @click="loadLastCode"
+            >Dernier code</q-btn
+          >
           <q-toolbar-title>PyRobotSim</q-toolbar-title>
         </q-toolbar>
       </q-header>
@@ -14,8 +22,33 @@
         :height="400"
         @change="change"
       />
+      <q-card>
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+        >
+          <q-tab name="stdout" label="Sortie" />
+          <q-tab name="stderr" label="Erreurs" />
+          <q-tab name="repl" label="REPL" />
+        </q-tabs>
 
-      <q-btn color="green" class="q-ma-sm">Exécuter</q-btn>
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="stdout">
+            <pre :key="stdout_counter">{{ stdout }}</pre>
+          </q-tab-panel>
+
+          <q-tab-panel name="stderr">
+            <pre>{{ stderr }}</pre>
+          </q-tab-panel>
+          <q-tab-panel name="repl"> REPL </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
     </template>
 
     <template v-slot:after
@@ -55,13 +88,17 @@ const cmOptions = {
 const props = defineProps({});
 const splitterLocation = ref(50);
 const code = ref("");
+const tab = ref("stdout");
+const stdout = ref("");
+const stdout_counter = ref(0);
 
 const pyodideLoaded = ref(false);
 const errorMsg = ref(null);
 
-const LOCAL_PYODIDE = false;
+let pyodide = null;
 
-console.log("loadscript", loadScript);
+const LOCAL_PYODIDE = false;
+const PYODIDE_VERSION = "v0.19.0";
 
 const initializePyodide = async () => {
   try {
@@ -69,22 +106,34 @@ const initializePyodide = async () => {
       window.languagePluginUrl = `${LOCAL_PYODIDE_SERVER_URL}`;
       await loadScript(`${LOCAL_PYODIDE_SERVER_URL}pyodide.js`);
     } else {
-      window.languagePluginUrl =
-        "https://cdn.jsdelivr.net/pyodide/v0.19.0/full/";
-      const res = loadScript(
-        "https://cdn.jsdelivr.net/pyodide/v0.19.0/full/pyodide.js"
+      window.languagePluginUrl = `https://cdn.jsdelivr.net/pyodide/${PYODIDE_VERSION}/full/`;
+      await loadScript(
+        `https://cdn.jsdelivr.net/pyodide/${PYODIDE_VERSION}/full/pyodide.js`
       );
-      res.then(() => {
-        console.log("plugin loader", window.languagePluginLoader);
-      });
-      console.log("res", res);
 
-      console.log("pyodide loading ...");
+      // Je ne sais pas encore comment gérer le stdin => le message se met dans le stdout ...
+      // bonnes infos sur https://github.com/pyodide/pyodide/issues/550#issuecomment-903182645
+
+      // autres ressources intéressantes pour gérer les entrées / sorties et
+      // pour tester du code Python (faire un test runner)
+      // https://github.com/magurofly/atcoder-easy-test/tree/main/v2/src
+      // Sujet : japanese coding contest site (code runners : Brython / Pyodide)
+
+      pyodide = await loadPyodide({
+        indexURL: languagePluginUrl,
+        stdin: window.prompt,
+        stdout: (text) => {
+          console.log(text);
+          stdout.value += text + "\n";
+          stdout_counter.value += 1;
+          console.log("stdout", stdout.value);
+        },
+        stderr: (text) => (stderr.value += text + "\n"),
+      });
+      console.log("pyodide loading ...", pyodide);
     }
-    // wait for pyodide ready
-    await window.languagePluginLoader;
     // load pandas lib
-    // await window.pyodide.loadPackage(["pandas", "scikit-learn"]);
+    // await pyodide.loadPackage(["pandas", "scikit-learn"]);
     pyodideLoaded.value = true;
   } catch (error) {
     console.log(error);
@@ -93,14 +142,26 @@ const initializePyodide = async () => {
 };
 
 const runTestCommand = () => {
-  console.log(window.pyodide.runPython(`import sys\nprint(sys.version)`));
+  console.log(pyodide.runPython(`import sys\nprint(sys.version)`));
+};
+
+const runCode = () => {
+  localStorage.setItem("lastCodeRun", code.value);
+  pyodide.runPython(code.value);
+};
+
+const loadLastCode = () => {
+  const contents = localStorage.getItem("lastCodeRun");
+  if (contents === undefined) {
+    contents = "";
+  }
+  code.value = contents;
 };
 
 onMounted(async () => {
   console.log("loading Pyodide");
   await initializePyodide();
-  console.log(window.pyodide);
-  runTestCommand();
+  stdout.value = "";
 });
 </script>
 
