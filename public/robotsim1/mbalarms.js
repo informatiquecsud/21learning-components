8620d802979198ee56e7f97627b676498ab332b3
Cédric Donner <cedonner@gmail.com>
2022-09-22 02:31:24 +0200

    fix issue when setAlarm(1) gets called multiple times

--- public/robotsim1/mbalarms.js
+++ public/robotsim1/mbalarms.js
@@ -1,11 +1,16 @@
 function createAlarm(duration) {
   var alarmInterval = undefined;
+  var isOn = false;
   function alarm(on) {
     // console.log("alarm state", on);
     if (on) {
-      alarmInterval = setInterval(() => playSound("bump"), 600);
+      if (!isOn) {
+        alarmInterval = setInterval(() => playSound("bump"), 600);
+        isOn = true;
+      }
     } else {
       clearInterval(alarmInterval);
+      isOn = false;
     }
   }
 
