4de54f60505dff10925784e1da39cd3c4359240c
Cédric Donner <cedonner@gmail.com>
2022-09-23 12:44:32 +0200

    small fix to bgFilledMark world

--- public/robotsim1/worlds/bgFilledMark.js
+++ public/robotsim1/worlds/bgFilledMark.js
@@ -10,18 +10,17 @@ function mapCreate(scene) {
   const [x, y, angle] = [0, 0, 0];
 
   robot = new maqueenLite(scene, "N°1", x, y, angle);
-  robot.setPosition(-230, 50);
-  robot.setAngle(-75);
+  robot.setPosition(-55, 118);
+  robot.setAngle(-30);
 
-  const scale = 0.5;
+  const scale = 0.3;
   const [picx, picy, picangle, scaleX, scaleY] = [0, 0, 0, scale, scale];
   const bg = new Picture(scene, "bg", picx, picy, picangle, scaleX, scaleY);
 }
 
 function sceneCreated({ overlayScene, robots }) {
   //  overlayScene.freeMode();
-  overlayScene.zoomOut();
-  overlayScene.zoomOut();
+  overlayScene.zoomIn();
 
   //overlayScene.camera.scrollX += 200;
   // robots[0].setPosition(0, 0);
